103d2a394a6ea68d6a12d74d8d301b0fa6f0f8e0
Cleiton Rieke <cleitonrieke@gmail.com>
2024-12-24 00:41:04 -0300

    primeiro commit

---
+++ src/pages/RegistradoPage.vue
@@ -0,0 +1,32 @@
+<template>
+  <q-page>
+    <div class="q-pa-md row justify-center">
+      <h6 class="text-white bg-pink-6">
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;BOLÃO MEGA
+        BROTHERS&nbsp;&nbsp;<br />&nbsp;&nbsp;Loto Fácil de Independência
+        2024&nbsp;&nbsp;
+      </h6>
+      <br />
+    </div>
+    <div class="q-pa-md row justify-center">
+      <div class="col-4"></div>
+    </div>
+    <div class="q-pa-md row justify-center">
+      <img
+        alt="Example Image"
+        src="~assets/happy_sun.png"
+        style="width: 400px; height: 400px"
+      />
+    </div>
+    <div class="q-pa-md row justify-center">
+      <div class="col-6">
+        <h5 class="text-pink-9">
+          Parabéns!!! Você está registrado no bolão. Aguarde contato com
+          instruções para realizar seu pagamento.
+        </h5>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script setup></script>
